833a01dd3bce394446f536c5eb080ea7154227ef
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 16:02:15 +0200

    refactor(test): remove Sass loader and simplify Webpack Encore configuration
    - Remove enableSassLoader() call from Webpack Encore configuration
    - Simplify chaining syntax by removing trailing semicolon
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- test-storybooks/symfony-webpack-encore-kitchen-sink/webpack.config.cjs
+++ test-storybooks/symfony-webpack-encore-kitchen-sink/webpack.config.cjs
@@ -4,16 +4,13 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-Encore
-  .setOutputPath('public/build/')
+Encore.setOutputPath('public/build/')
   .setPublicPath('/build')
   .addEntry('app', './assets/app.js')
   .splitEntryChunks()
   .enableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
-  .enableVersioning(Encore.isProduction())
-  .enableSassLoader()
-;
+  .enableVersioning(Encore.isProduction());
 
 module.exports = Encore.getWebpackConfig();
