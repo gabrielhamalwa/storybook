3ec81220e9e9cd8169348dbefe50e75dd374be0d
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:55:20 +0200

    feat(symfony): remove autodocs tag export from entry-preview
    - Remove tags export with autodocs from entry-preview.ts
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- code/renderers/symfony/src/entry-preview.ts
+++ code/renderers/symfony/src/entry-preview.ts
@@ -1,7 +1,5 @@
 import type { Parameters } from './types.ts';
 
-export const tags = ['autodocs'];
-
 export const parameters: Parameters = { renderer: 'symfony' };
 
 export { render, renderToCanvas } from './render.ts';
