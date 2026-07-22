d9352eb1034c23f465665d54202e91776edc63f3
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:56:16 +0200

    refactor(symfony): add WASM archive URL and PHP-WASM module type declarations
    - Add STORYBOOK_SYMFONY_ARCHIVE_URL to ImportMetaEnv interface
    - Add module declaration for @php-wasm/web-8-4 with getIntlExtensionPath and getPHPLoaderModule exports
    - Import loadPHPRuntime type from @php-wasm/universal for getPHPLoaderModule return type
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- code/renderers/symfony/src/typings.d.ts
+++ code/renderers/symfony/src/typings.d.ts
@@ -1,7 +1,15 @@
 declare var STORYBOOK_ENV: 'SYMFONY';
 
 declare interface ImportMetaEnv {
+  STORYBOOK_SYMFONY_ARCHIVE_URL?: string;
   STORYBOOK_SYMFONY_URL?: string;
+}
+
+declare module '@php-wasm/web-8-4' {
+  import { type loadPHPRuntime } from '@php-wasm/universal';
+
+  export function getIntlExtensionPath(): Promise<string>;
+  export function getPHPLoaderModule(): Promise<Parameters<typeof loadPHPRuntime>[0]>;
 }
 
 declare interface ImportMeta {
