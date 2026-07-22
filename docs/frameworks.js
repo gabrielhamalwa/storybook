14056b101effe9edaec9d70c0e14a8fad34c9fc8
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:58:12 +0200

    refactor(docs): move symfony from communityFrameworks to coreFrameworks
    - Move symfony from communityFrameworks array to coreFrameworks array
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- docs/frameworks.js
+++ docs/frameworks.js
@@ -1,6 +1,6 @@
 module.exports = {
-  coreFrameworks: ['react', 'vue', 'angular', 'web-components'],
-  communityFrameworks: ['ember', 'html', 'svelte', 'preact', 'qwik', 'solid', 'symfony'],
+  coreFrameworks: ['react', 'vue', 'angular', 'web-components', 'symfony'],
+  communityFrameworks: ['ember', 'html', 'svelte', 'preact', 'qwik', 'solid'],
   featureGroups: [
     {
       name: 'Essentials',
