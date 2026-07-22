a714e424b1054501013457ba1436c2005810dbbc
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:56:36 +0200

    refactor(symfony): remove asset module exports from index
    - Remove assetMapper, encore, none, pentatrionVite, and stimulus module exports
    - Keep only inject and types module exports
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- code/renderers/symfony/src/assets/index.ts
+++ code/renderers/symfony/src/assets/index.ts
@@ -1,7 +1,2 @@
-export * from './assetMapper.ts';
-export * from './encore.ts';
 export * from './inject.ts';
-export * from './none.ts';
-export * from './pentatrionVite.ts';
-export * from './stimulus.ts';
 export * from './types.ts';
