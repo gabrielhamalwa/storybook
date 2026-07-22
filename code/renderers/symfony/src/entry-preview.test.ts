10cff043885df7815ee0e033841d6b015902396d
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:55:15 +0200

    test(symfony): remove autodocs tag test from entry-preview tests
    - Remove tags import from entry-preview.ts
    - Remove test verifying autodocs tag is enabled by default
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- code/renderers/symfony/src/entry-preview.test.ts
+++ code/renderers/symfony/src/entry-preview.test.ts
@@ -1,12 +1,8 @@
 import { describe, expect, it } from 'vitest';
 
-import { parameters, tags } from './entry-preview.ts';
+import { parameters } from './entry-preview.ts';
 
 describe('entry-preview', () => {
-  it('enables autodocs by default', () => {
-    expect(tags).toEqual(['autodocs']);
-  });
-
   it('exports the symfony renderer parameter', () => {
     expect(parameters).toMatchObject({ renderer: 'symfony' });
   });
