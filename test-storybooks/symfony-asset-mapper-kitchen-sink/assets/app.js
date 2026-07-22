23a3cec07903e98d0184798786a47fe9f56018b8
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 15:59:54 +0200

    refactor(test): register LiveController in Stimulus application
    - Import LiveController from @symfony/ux-live-component
    - Register LiveController with 'live' identifier in Stimulus application
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- test-storybooks/symfony-asset-mapper-kitchen-sink/assets/app.js
+++ test-storybooks/symfony-asset-mapper-kitchen-sink/assets/app.js
@@ -1,6 +1,8 @@
 import { Application } from '@hotwired/stimulus';
+import LiveController from '@symfony/ux-live-component';
 import ButtonController from './controllers/button_controller.js';
 
 const application = Application.start();
 application.debug = false;
 application.register('button', ButtonController);
+application.register('live', LiveController);
