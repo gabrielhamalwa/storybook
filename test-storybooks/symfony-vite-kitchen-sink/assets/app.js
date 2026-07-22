f8258b68be4d08cfe5986bbf5eb7e63d2a592715
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 16:01:08 +0200

    refactor(test): register LiveController in Stimulus application
    - Import LiveController from @symfony/ux-live-component
    - Register LiveController with 'live' identifier in Stimulus application
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- test-storybooks/symfony-vite-kitchen-sink/assets/app.js
+++ test-storybooks/symfony-vite-kitchen-sink/assets/app.js
@@ -1,7 +1,9 @@
 import { Application } from '@hotwired/stimulus';
+import LiveController from '@symfony/ux-live-component/dist/live_controller.js';
 import { registerControllers } from 'vite-plugin-symfony/stimulus/helpers';
 
 const app = Application.start();
+app.register('live', LiveController);
 
 registerControllers(
   app,
