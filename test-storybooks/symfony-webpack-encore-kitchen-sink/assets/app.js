c788827bdedfbef17d8b3216d9f8d572cc4b6cdd
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 16:02:21 +0200

    feat(test): add Symfony Live Component controller registration to Stimulus application
    - Import LiveController from @symfony/ux-live-component
    - Register 'live' controller with Stimulus application for Live Component functionality
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- test-storybooks/symfony-webpack-encore-kitchen-sink/assets/app.js
+++ test-storybooks/symfony-webpack-encore-kitchen-sink/assets/app.js
@@ -1,8 +1,10 @@
 import './styles/app.css';
 
 import { Application } from '@hotwired/stimulus';
+import LiveController from '@symfony/ux-live-component/dist/live_controller.js';
 import ButtonController from './controllers/button_controller.js';
 
 const application = Application.start();
 application.debug = false;
 application.register('button', ButtonController);
+application.register('live', LiveController);
